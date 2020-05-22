da1491710caac0fd11bee28e9105d0fa094960ae
unknown <leydianealves24@hotmail.com>
2020-05-22 17:07:01 +0100

    checkout functionality with orders and orderitems models

---
+++ app/Models/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\User;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'order_number', 'user_id', 'status', 'grand_total', 'item_count',
+        'payment_status', 'first_name', 'last_name', 'address', 'city',
+        'country', 'post_code', 'phone_number', 'notes'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
